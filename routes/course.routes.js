99046a38a86105ba3757311cfb86800efaaf3319
Ricard05 <ricardomtzlp05@gmail.com>
2025-03-25 22:04:34 -0600

    feat: fix sequalize

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import Course from "../models/Course.js";
 import { verifyToken } from "../config/jwt.js";
 import { Op } from "sequelize";
+import sequelize from "../config/db.js";
 
 const router = express.Router();
 
